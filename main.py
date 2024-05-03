70c20683913fb77af7258f39426ed445d6e731e3
vinicius amancio da da silva <vinicius.amancio4257@gmail.com>
2024-05-03 08:50:47 -0300

    importação dos arquivos

---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, session, redirect, request
+from usuario import Usuario
+import sqlite3
+
+
+
+
+app = Flask(__name__)
+conexao = sqlite3.connect("shopbck.sqlite")
+cursor = conexao.cursor()
+cursor.execute()
+conexao.commit()
+conexao.close()
+
+# Define cadastro route
+@app.route("/cadastro", methods = ["GET", "POST"])
+def cadastro():
+    if request.method == 'GET':
+        return render_template("cadastro.html")
+    else:
+        nome = request.form["nome"]
+        cpf = request.form["cpf"]
+        telefone = request.form["telefone"]
+        endereco = request.form["endereco"]
+        numero = request.form["numero"]
+        email = request.form["email"]
+        cpf = request.form["cpf"]
+        conexao = sqlite3.connect("shopbck.sqlite")
+        cursor = conexao.cursor()
+        cursor.execute(f"INSERT INTO tb_cliente VALUES('{nome}', {cpf}, '{telefone}', '{endereco}, '{numero}', '{email}')")
+        conexao.commit()
+        conexao.close()
+
+        return render_template("logar.html")
+    
+@app.route('/logar', methods = ['GET','POST'])
+def logar():
+    if request.method =='GET':
+        return render_template('login.html')
+    else:
+        senha = request.form['senha']
+        email = request.form['email']
+        usuario = Usuario()
+        usuario.logar(email, senha)
+        if  usuario.logado == True:
+            session['usuario_logado'] = {"nome": usuario.nome,
+                                    "email": usuario.email}
+            return redirect("/index")
+            
+        else:
+            session.clear()
+            return redirect("/logar")
+        
---
+++ usuario.py
@@ -0,0 +1,42 @@
+from conexao import Conexao
+from hashlib import sha256
+
+class Usuario:
+    def __init__(self):
+        self.tel = None
+        self.nome = None
+        self.senha = None
+        self.logado = False
+    def cadastrar(self, telefone, nome, senha):
+        senha = sha256(senha.encode()).hexdigest()
+        try:
+            mydb = Conexao.conectar()
+
+
+            mycursor = mydb.cursor()
+
+            
+            sql = f"INSERT INTO tb_usuario (tel, nome, senha) VALUES ('{telefone}', '{nome}', '{senha}')"
+            
+            mycursor.execute(sql)
+            
+            self.tel = telefone
+            self.nome = nome
+            self.senha = senha
+            self.logado = True
+
+
+            mydb.commit()
+
+            print(mycursor.rowcount, "registro inserido")
+
+            mycursor.execute("SELECT tel, nome FROM tb_usuario")
+
+            ver = mycursor.fetchall()
+
+            print(ver)
+
+            mydb.close()
+            return True
+        except:
+            return False
